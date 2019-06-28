579dfe2b48088980b948abc62089083d05dce03b
Amol Aher <amola@winjit.com>
2019-06-28 11:47:05 +0530

    OCR implementation Added

--- PdfForm/Controllers/HomeController.cs
+++ PdfForm/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
@@ -79,8 +80,8 @@ namespace PdfForm.Controllers
 
             var path = PdfHelper.GeneratePdfFromExisingValues(list);
             if (path == null)
-               return Content("filename not present");
-           
+                return Content("filename not present");
+
             var memory = new MemoryStream();
             using (var stream = new FileStream(path, FileMode.Open))
             {
@@ -122,5 +123,62 @@ namespace PdfForm.Controllers
                 {".csv", "text/csv"}
             };
         }
+
+
+        [HttpGet]
+        public IActionResult OCrTest(InitialLoadModel model)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> OCrTest(List<IFormFile> files)
+        {
+            byte[] image = null;
+
+            foreach (var file in files)
+            {
+                if (file.Length > 0)
+                {
+                    using (var ms = new MemoryStream())
+                    {
+                        file.CopyTo(ms);
+                        image = ms.ToArray();
+                        // string s = Convert.ToBase64String(image);
+                        // act on the Base64 data
+                    }
+                }
+            }
+
+            HttpClient client = new HttpClient();
+            HttpContent bytesContent = new ByteArrayContent(image);
+            
+            var contentdata = new FormUrlEncodedContent(new[]
+            {
+                 new KeyValuePair<string, string>("Type", "SA_DL"),
+            });
+            
+            var formdata = new MultipartFormDataContent();
+            formdata.Add(bytesContent);
+            formdata.Add(contentdata);
+
+            var request = new HttpRequestMessage
+            {
+                Content = formdata,
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("http://52.172.133.91:5023/show"),
+            };
+
+            client.DefaultRequestHeaders.Add("Postman-Token", "0b2b8d5e-ac04-420e-96e7-ee60806a75a9");
+            var response = await client.SendAsync(request);
+            var data = await response.Content.ReadAsStringAsync();
+            return Ok(new OCRResult { Message = "Result Return Success", response = data });
+        }
+    }
+
+    public class OCRResult
+    {
+        public string Message { get; set; }
+        public object response { get; set; }
     }
 }
--- PdfForm/Startup.cs
+++ PdfForm/Startup.cs
@@ -49,7 +49,7 @@ namespace PdfForm
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+         //   app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
